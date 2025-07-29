1013e8266e7c01f1c79a16c9ef197e38d88fe341
Danish <danish.tapia@combatrobotics.in>
2025-07-29 18:31:44 +0530

    savepoint
    - Attempted to Integrate the Libraries given by Infineon

---
+++ SPI_BGT60TR13C/Debug/Core/Src/xensiv_bgt60trxx.d
@@ -0,0 +1,6 @@
+Core/Src/xensiv_bgt60trxx.o: ../Core/Src/xensiv_bgt60trxx.c \
+ ../Core/Inc/xensiv_bgt60trxx.h ../Core/Inc/xensiv_bgt60trxx_regs.h \
+ ../Core/Inc/xensiv_bgt60trxx_platform.h
+../Core/Inc/xensiv_bgt60trxx.h:
+../Core/Inc/xensiv_bgt60trxx_regs.h:
+../Core/Inc/xensiv_bgt60trxx_platform.h:
